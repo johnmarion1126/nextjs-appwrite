24ff532ae4309283d4ee833a33a8e9a867244a17
johnmsanchez <johnmarion1126@gmail.com>
2023-09-17 12:14:02 -0700

    create signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,74 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
+import { Axios } from "axios";
+
 export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {
+
+  }
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-2">
       <h1>Signup</h1>
+      <hr />
+
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({
+          ...user,
+          username: e.target.value
+        })}
+        placeholder="username"
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({
+          ...user,
+          email: e.target.value
+        })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({
+          ...user,
+          password: e.target.value
+        })}
+        placeholder="password"
+      />
+
+      <button
+        type="button"
+        onClick={onSignup}
+        className="p-2 border border-gray-300 mt-4"
+      >
+        Signup Here
+      </button>
+      <Link href="/login">
+        Visit Login Page
+      </Link>
     </div>
   )
 }
